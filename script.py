05082fe85a263eda8244c8c80795501341f1c212
DrShams <sithis02@mail.ru>
2022-09-17 13:24:43 +0500

    aiogram

--- script.py
+++ script.py
@@ -1,44 +1,83 @@
 from pytube import Playlist
 import os
 import sqlite3
+import youtube_dl
+import re
+
+
+STATUS_CODE_JUST_DOWNLOADED = 0
 path = os.path.abspath("files/")
 os.environ["PATH"] += os.pathsep + path
-
 conn = sqlite3.connect('files/db.sqlite',check_same_thread=False)
 cur = conn.cursor()
-cur.executescript('''
-    CREATE TABLE IF NOT EXISTS "Users" (
-	"id" INTEGER,
-	"user_name" TEXT,
-	"utime_checked"	INTEGER);
 
-    CREATE TABLE IF NOT EXISTS "Playlist" (
-    "id" INTEGER,
-	"user_id" INTEGER,
-    "url" TEXT,
-    "status" INTEGER,
-    PRIMARY KEY("id" AUTOINCREMENT));
+def makedb():
+    """Create Database if not exists"""
 
-    CREATE TABLE IF NOT EXISTS "Userplaylist" (
-	"id" INTEGER,
-    "user_id" INTEGER,
-	"user_name"	TEXT,
-	"utime_checked"	INTEGER),
-    PRIMARY KEY("id" AUTOINCREMENT));'''
-)
+    cur.executescript('''
+        CREATE TABLE IF NOT EXISTS "Users" (
+        	"id" INTEGER,
+        	"user_name" TEXT,
+        	"playlist_url"	TEXT);
 
+        CREATE TABLE IF NOT EXISTS "Playlist" (
+            "id" INTEGER,
+        	"user_id" INTEGER,
+            "url" TEXT,
+            "status" INTEGER,
+        PRIMARY KEY("id" AUTOINCREMENT));
+        '''
+    )
 
-link = Playlist('https://www.youtube.com/playlist?list=PLFaKDBYWwxzTht5OKenNoWWWkMfmAh5NV')
-os.chdir('files')
-for url in link.video_urls:
-    print(url)
-    cur.execute('SELECT id FROM Playlist WHERE url = ?',(url,))
-    row = cur.fetchone()
-    conn.commit()
-    if row is None:
-        os.system('youtube-dl -x --extract-audio --audio-format mp3 --output %(uploader)s%(title)s.%(ext)s ' + url)
-        cur.execute('INSERT INTO Playlist (url, status) VALUES (?, ?)',(url, 0))#update status 1 when it will be deployed
+def download():
+    """Download all files in mp3 format from youtube by defined link"""
+
+    link = Playlist('https://www.youtube.com/playlist?list=PLFaKDBYWwxzTht5OKenNoWWWkMfmAh5NV')
+    os.chdir('files')
+
+    options = {
+        'format': 'bestaudio/best',
+        'extractaudio' : True,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }]
+    }
+
+    ydl = youtube_dl.YoutubeDL(options)
+    for url in link.video_urls:
+        print(url)
+
+        cur.execute('SELECT id FROM Playlist WHERE url = ?',(url,))
+        row = cur.fetchone()
         conn.commit()
-    else:
-        id = row[0]
-        print(url, "is already in the database with id=",id)
+        if row is None:
+            with ydl:#GOOGLE
+                result = ydl.extract_info(
+                    url,
+                    download = True
+                )
+            if 'entries' in result:
+                # Can be a playlist or a list of videos
+                video = result['entries'][0]
+            else:
+                # Just a video
+                video = result
+            video_title = video['title']
+
+            pattern = '=([\w]*)'
+            m = re.search(pattern,url)
+
+            filename = video_title + '-' + m.group(1)
+            print(filename)
+
+            cur.execute('INSERT INTO Playlist (url, status) VALUES (?, ?)',(url, STATUS_CODE_JUST_DOWNLOADED))#update status 1 when it will be deployed
+            conn.commit()
+        else:
+            id = row[0]
+            #print(url, "is already in the database with id=",id)
+
+if __name__ == "__main__":
+    makedb()
+    download()
--- tel.py
+++ tel.py
@@ -1,13 +1,99 @@
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
+import sqlite3
+conn = sqlite3.connect('files/db.sqlite',check_same_thread=False)
+cur = conn.cursor()
 
 import configparser  # импортируем библиотеку
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
+
+# --- Main Menu ---
+#Создаем кнопки
+btn_main = KeyboardButton('Главное меню')
+btn_music = KeyboardButton('Музыка') # скачивание музыки с youtube
+btn_travel = KeyboardButton('Турагентство') # кнопка для вызова парсера для турагенства и публикации в вк
+btn_camera = KeyboardButton('Камера') # кнопка для вызова камеры
+btn_english = KeyboardButton('Английский') # кнопка для вызова парсера
+
+main_menu = ReplyKeyboardMarkup(resize_keyboard = True) # чтобы кнопки не были во весь экран
+main_menu.add(btn_music, btn_travel, btn_camera, btn_english, btn_main)
+
+
+# --- Full Music menu ---
+btn_music_download = KeyboardButton('Скачать')
+btn_music_change = KeyboardButton('Редактировать плейлист')
+music_menu = ReplyKeyboardMarkup(resize_keyboard = True)
+music_menu.add(btn_music_download, btn_music_change)
+
+# --- Shorted Music menu ---
+music_smenu = ReplyKeyboardMarkup(resize_keyboard = True)
+music_smenu.add(btn_music_change)
+
+
 config = configparser.ConfigParser()  # создаём объекта парсера
 config.read("settings.ini")  # читаем конфиг
 TOKEN = config["Telegram"]["TOKEN"] # обращаемся как к обычному словарю!
 print(TOKEN)
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-print(bot)
+
+# @dp.callback_query_handler(text = 'button1')
+# async def process_callback_button1(callback_query: types.CallbackQuery):
+#     await bot.answer_callback_query(callback_query.id)
+#     await bot.send_message(callback_query.from_user.id, 'Нажата первая кнопка!')
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    """This handler will be called when user sends `/start`"""
+    user_id = message.from_user.id
+    first_name = message.from_user.first_name
+    #проверим есть ли пользователь в базе
+    cur.execute('SELECT * FROM Users WHERE id = ?',(user_id,))
+    row = cur.fetchone()
+    conn.commit()
+    if row is None:
+        cur.execute('INSERT INTO Users (id, user_name) VALUES (?, ?)',(user_id, first_name))#update status 1 when it will be deployed
+        conn.commit()
+        await bot.send_message(message.from_user.id, "Вы были записаны базу данных")
+    else:
+        print("Пользователь " + first_name + " вошел")
+    await message.reply("Привет!\nНапиши мне что-нибудь!",reply_markup=main_menu)
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    """This handler will be called when user sends `/help`"""
+    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+
+@dp.message_handler()
+async def echo_message(message: types.Message):
+    """This handler will be called when user sends any other command"""
+    user_id = message.from_user.id
+
+    if message.text == 'Музыка':
+        #проверим есть ли ссылка на плейлист для этого пользователя
+        cur.execute('SELECT playlist_url FROM Users WHERE id = ? AND playlist_url is not NULL',(user_id,))
+        row = cur.fetchone()
+        conn.commit()
+        if row is None:
+            await bot.send_message(message.from_user.id, "Вставьте ссылку в чат на плейлист youtube")
+        else:
+            await bot.send_message(message.from_user.id, "Плейлист найден url = " + str(row[0]))#Остановился здесь предлагаем кнопку скачать
+    else:
+        if 'https://www.youtube.com/playlist?list' in message.text:
+            link = message.text
+            cur.execute('UPDATE Users SET playlist_url = ? WHERE id = ?',(link,user_id))
+            row = cur.fetchone()
+            conn.commit()
+            await bot.send_message(message.from_user.id, "Ссылка была успешно добавлена " + link)
+        else:
+            await bot.send_message(message.from_user.id, message.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
