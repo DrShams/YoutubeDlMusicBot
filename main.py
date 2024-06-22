c3c07e42f85b334527bd9aa56c30c2fb89cafa5c
DrShams <sithis02@mail.ru>
2024-06-22 15:53:20 +0300

    add logging

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os
-import logging
-from utils.logging_config import configure_logging
-configure_logging()
 from aiogram import executor
 from bot import dp
 from utils import database, buttons
+from utils.logging_config import configure_logging
+configure_logging()
 
 import handlers.commands
 import handlers.echo
@@ -12,8 +11,6 @@ import handlers.echo
 if __name__ == '__main__':
     path = os.path.abspath("files/")
     os.environ["PATH"] += os.pathsep + path
-
-    logging.basicConfig(level=logging.DEBUG, filename = "files/logs.log", filemode="w")
 
     database.makedb()
     buttons.createbuttons()
